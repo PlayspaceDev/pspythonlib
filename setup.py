2a423761a34dee8531c8d18d02887638d75f44b2
moss <moritz@playspace.com>
2019-05-20 09:58:45 +0200

    Another git remote branch check fix

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pspylib",
-    version="0.0.11",
+    version="0.0.12",
     description="Playspace shared python library",
     long_description=long_description,
     long_description_content_type="text/markdown",
